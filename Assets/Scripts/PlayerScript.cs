dd234a54de764aacd1b96144259352b2dcd91a00
Djelal BOUDJI <djelal2004@gmail.com>
2024-12-16 11:41:07 +0100

    feat: add particles on player if it is not jumping

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,6 +7,8 @@ public class PlayerScript : MonoBehaviour
 
     public Vector3 initialPosition;
 
+    public ParticleSystem particleSystem;
+
     public void Start()
     {
         initialPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
@@ -16,17 +18,29 @@ public class PlayerScript : MonoBehaviour
     {
         transform.position += Time.deltaTime * 8.6f * Vector3.right;
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (!IsJumping())
         {
-            if (!IsJumping())
+            if (Input.GetKeyDown(KeyCode.Space))
             {
                 rigidBody.AddForce(Vector2.up * 26.6581f, ForceMode2D.Impulse);
             }
+            particleSystem.gameObject.SetActive(true);
         }
+        else
+        {
+            particleSystem.gameObject.SetActive(false);
+        }
+        ParticleSystemSpeed();
     }
 
     private bool IsJumping()
     {
         return Mathf.Abs(initialPosition.y - transform.position.y) > 0.05f;
+    }
+
+    private void ParticleSystemSpeed()
+    {
+        var velocityOverLifetime = particleSystem.velocityOverLifetime;
+        velocityOverLifetime.x = rigidBody.linearVelocity.x;
     }
 }
