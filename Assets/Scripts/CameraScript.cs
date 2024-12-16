3e719a8d67f73ff9c785705ff0d5a1ba9fea505c
Théo LUDWIG <contact@theoludwig.fr>
2024-12-16 11:01:50 +0100

    feat: place cube and jump + move camera (#24)

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject playerObject;
+
+    public void Update()
+    {
+        transform.position = new Vector3(playerObject.transform.position.x, transform.position.y, transform.position.z);
+        // transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+    }
+}
