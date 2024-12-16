a7c19a65498a46cbaf11ea75979b7e899dfa45a3
djelalb <122933761+djelalb@users.noreply.github.com>
2024-12-16 11:59:21 +0100

    feat: add particles on player if it is not jumping (#25)

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,26 +7,54 @@ public class PlayerScript : MonoBehaviour
 
     public Vector3 initialPosition;
 
+    public ParticleSystem particleSystem;
+
     public void Start()
     {
         initialPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+
+        var mainModule = particleSystem.main;
+        mainModule.simulationSpace = ParticleSystemSimulationSpace.World;
+
+        particleSystem.transform.parent = null;
     }
 
     public void Update()
     {
         transform.position += Time.deltaTime * 8.6f * Vector3.right;
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (!IsJumping())
         {
-            if (!IsJumping())
+            Vector3 Rotation = transform.rotation.eulerAngles;
+            Rotation.z = Mathf.Round(Rotation.z / 90) * 90;
+            transform.rotation = Quaternion.Euler(Rotation);
+
+            if (Input.GetKeyDown(KeyCode.Space))
             {
                 rigidBody.AddForce(Vector2.up * 26.6581f, ForceMode2D.Impulse);
             }
+            particleSystem.gameObject.SetActive(true);
         }
+        else
+        {
+            particleSystem.gameObject.SetActive(false);
+            transform.Rotate(Vector3.back * 360 * Time.deltaTime);
+        }
+
+        particleSystem.transform.position = transform.position + new Vector3(-0.19f, -0.64f, 0);
+        particleSystem.transform.rotation = Quaternion.Euler(0, 0, 150.464f);
+
+        ParticleSystemSpeed();
     }
 
     private bool IsJumping()
     {
         return Mathf.Abs(initialPosition.y - transform.position.y) > 0.05f;
+    }
+
+    private void ParticleSystemSpeed()
+    {
+        var velocityOverLifetime = particleSystem.velocityOverLifetime;
+        velocityOverLifetime.x = rigidBody.linearVelocity.x;
     }
 }
