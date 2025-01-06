1779e3cf486637e1ee393578bbc1d5c24601fca6
Vincent PETT <pett.vincent@gmail.com>
2025-01-06 15:57:51 +0100

    feat: add-background

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -80,7 +80,7 @@ public class PlayerScript : MonoBehaviour
 
     private void UpdateParticlePositionAndRotation()
     {
-        particle.transform.position = transform.position + new Vector3(-0.19f, -0.64f, 0);
+        particle.transform.position = transform.position + new Vector3(-0.19f, -0.64f, -10);
         particle.transform.rotation = Quaternion.Euler(0, 0, 150.464f);
     }
 
