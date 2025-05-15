1653b9062378960c6b398db7fc9a83b8f2bb9496
Vincent PETT <pett.vincent@gmail.com>
2025-05-15 21:18:34 +0200

    feat: add edit map on level editor

--- Assets/Scripts/LevelsSelect/LevelHomeButton.cs
+++ Assets/Scripts/LevelsSelect/LevelHomeButton.cs
@@ -5,6 +5,8 @@ public class LevelHomeButton : MonoBehaviour
 {
     public void GoToHome()
     {
+        PlayerPrefs.SetInt("CreateMode", 0);
+        PlayerPrefs.SetInt("EditMode", 0);
         SceneManager.LoadScene("HomeScene");
     }
 }
