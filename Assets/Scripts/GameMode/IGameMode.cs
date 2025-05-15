bde6ea3a433159c06217e2b3389a68c5e2721e97
Théo LUDWIG <contact@theoludwig.fr>
2025-05-15 07:12:46 +0200

    feat: init AI (#60)

--- Assets/Scripts/GameMode/IGameMode.cs
+++ Assets/Scripts/GameMode/IGameMode.cs
@@ -5,4 +5,6 @@ public interface IGameMode
     void Update(Player player);
     void OnCollisionEnter(Player player, Collision2D collision);
     void OnCollisionExit(Player player, Collision2D collision);
+
+    void Jump(Player player);
 }
